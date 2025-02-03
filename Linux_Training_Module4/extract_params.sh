8c6323b32f44ab1dde26ac64b9b904080704785c
Deepak19072004 <deepakraj.dharmalingam@gmail.com>
2025-02-03 12:45:18 +0530

    Updated Extract_params.sh file

--- Linux_Training_Module4/extract_params.sh
+++ Linux_Training_Module4/extract_params.sh
@@ -12,9 +12,9 @@ output="output.txt"
 >output.txt
 
 awk '
-/frame.time/ {print "\"frame.time\": " substr($0, index($0, $2)) >> "'"$output"'"}
-/wlan.fc.type/ {print "\"wlan.fc.type\": " $2 >> "'"$output"'"}
-/wlan.fc.subtype/ {print "\"wlan.fc.subtype\": " $2 >> "'"$output"'"}
-' "$input" | sed '/,$/d' >> "$output"
+/"frame.time"/ {print "\"frame.time\": " substr($0, index($0, $2)) >> "'"$output"'"}
+/"wlan.fc.type"/ {print "\"wlan.fc.type\": " $2 >> "'"$output"'"}
+/"wlan.fc.subtype"/ {print "\"wlan.fc.subtype\": " $2 >> "'"$output"'"}
+' "$input"
 
 echo "Ouput saved in $output"
